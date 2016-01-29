39618855797723946468f8e6018e7e791f9a1ee8
till straumann <till.straumann@alumni.tu-berlin.de>
2016-01-28 19:03:59 -0800

    - added test for gpiolib

---
+++ gpiotst.c
@@ -0,0 +1,32 @@
+#include <gpiolib.h>
+#include <stdio.h>
+
+int
+main(int argc, char **argv)
+{
+gpio_handle gpio = gpio_open(0, EMIO_PIN);
+int         got;
+int         i;
+
+	if ( ! gpio )
+		return 1;
+
+	if ( gpio_out(gpio) ) {
+		perror("gpio_out");
+		return 1;
+	}
+
+	for ( i=0; i<5; i++ ) {
+		if ( gpio_clr(gpio) ) {
+			perror("gpio_clr");
+			return 1;
+		}
+		sleep(1);
+		if ( gpio_set(gpio) ) {
+			perror("gpio_set");
+			return 1;
+		}
+		sleep(1);
+	}
+	return 0;
+}
